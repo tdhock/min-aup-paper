814959fd55245c33d25b6a51bf3b3eddaca19774
Toby Dylan Hocking <tdhock5@gmail.com>
2021-07-06 13:15:59 -0400

    timing figure

---
+++ figure-timing-grad-squared-hinge.R
@@ -0,0 +1,50 @@
+library(data.table)
+library(ggplot2)
+timing.dt <- data.table::fread("figure-timing-grad-squared-hinge-data.csv")
+stats.dt <- timing.dt[, .(
+  max=max(seconds),
+  median=median(seconds),
+  min=min(seconds)
+), by=.(N, prediction.order, algorithm)]
+
+func.only <- stats.dt[algorithm=="Functional"]
+gg <- ggplot()+
+  facet_grid(. ~ algorithm, labeller=label_both)+
+  geom_line(aes(
+    N, median, color=prediction.order),
+    data=func.only)+
+  geom_ribbon(aes(
+    N, ymin=min, ymax=max, fill=prediction.order),
+    alpha=0.5,
+    data=func.only)+
+  scale_x_log10(
+    "Number of labeled examples = elements in gradient vector",
+    limits=func.only[, c(min(N), max(N)*10)])+
+  scale_y_log10(
+    "Time to compute gradient (seconds),
+median line and min/max band over 10 timings")
+dl <- directlabels::direct.label(gg, "right.polygons")
+png("figure-timing-grad-squared-hinge-sorted.png", width=6, height=4, units="in", res=200)
+print(dl)
+dev.off()
+
+one <- stats.dt[prediction.order=="unsorted"]
+gg <- ggplot()+
+  geom_line(aes(
+    N, median, color=algorithm),
+    data=one)+
+  geom_ribbon(aes(
+    N, ymin=min, ymax=max, fill=algorithm),
+    alpha=0.5,
+    data=one)+
+  scale_x_log10(
+    "Number of labeled examples = elements in gradient vector",
+    limits=one[, c(min(N), max(N)*10)])+
+  scale_y_log10(
+    "Time to compute gradient (seconds),
+median line and min/max band over 10 timings")
+dl <- directlabels::direct.label(gg, "right.polygons")
+png("figure-timing-grad-squared-hinge.png", width=6, height=4, units="in", res=200)
+print(dl)
+dev.off()
+
